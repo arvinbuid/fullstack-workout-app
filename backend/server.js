c2370ceef9d3125fd48fb917b22aedf6724cb639
Arvin Buid <arvinbuid01@gmail.com>
2024-06-06 16:09:00 +0800

    Imported mongoose inside server.js. Initiate backend and mongodb connection, and added mongodb connection string.

--- backend/server.js
+++ backend/server.js
@@ -4,20 +4,29 @@ require("dotenv").config();
 const express = require("express");
 const port = process.env.PORT || 5000;
 const workoutRoutes = require("./routes/workouts.js");
+const mongoose = require("mongoose");
 
 // express app
 const app = express();
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
+// connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(port, () => {
+      console.log("connected to mongodb and app is listening on port ", port);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 // workout routes
 app.use("/api/workouts", workoutRoutes);
-
-app.listen(port, () => {
-  console.log("app is listening on port ", port);
-});
