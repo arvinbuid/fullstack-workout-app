d8ac74da85f603deb525bf62a6c9106ee13b72f3
Arvin Buid <arvinbuid01@gmail.com>
2024-06-10 19:00:15 +0800

    Imported the validator package inside userModel.js and use it to validate the email if it is valid and if the password is strong enough or not.

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = new Schema({
   email: {
@@ -16,6 +17,23 @@ const userSchema = new Schema({
 
 // signup and password hash
 userSchema.statics.signup = async function (email, password) {
+  // validation
+
+  // check if email && password is null
+  if (!email || !password) {
+    throw new Error("Please fill in all required fields.");
+  }
+
+  // check if valid email
+  if (!validator.isEmail(email)) {
+    throw new Error("Enter a valid email address.");
+  }
+
+  // check if strong password
+  if (!validator.isStrongPassword(password)) {
+    throw new Error("Password is not strong enough.");
+  }
+
   const userEmailExists = await this.findOne({email});
 
   // check if user email exists
