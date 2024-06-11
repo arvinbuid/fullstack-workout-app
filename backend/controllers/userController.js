574fa6585bed1c88141bd005861e966120379271
Arvin Buid <arvinbuid01@gmail.com>
2024-06-11 13:43:56 +0800

    Installed jsonwebtoken third party package to create and sign a token when the user sign up an account. Created createToken function inside userController and use it inside userSignup(). Added a secret key inside .env file.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({_id}, process.env.SECRET, {expiresIn: "3d"});
+};
 
 // user login
 const userLogin = async (req, res) => {
@@ -12,8 +17,11 @@ const userSignup = async (req, res) => {
   try {
     const user = await User.signup(email, password);
 
+    // create signup token
+    const token = createToken(user._id);
+
     // if success
-    res.status(201).json({email, user});
+    res.status(201).json({email, token});
   } catch (error) {
     res.status(400).json({error: error.message});
   }
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -52,3 +52,4 @@ userSchema.statics.signup = async function (email, password) {
 };
 
 module.exports = mongoose.model("User", userSchema);
+  
