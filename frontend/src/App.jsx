2f6662c6996fa58c15275f831e3c69f0b65d1072
Arvin Buid <arvinbuid01@gmail.com>
2024-06-13 15:58:42 +0800

    Changed the routing style from createBrowserRouter to BrowserRouter to conditionally render routes into the frontend based of user availability condition.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,25 @@
-import {Outlet} from "react-router";
+import {Navigate, Outlet, Route, Routes} from "react-router";
 import Navbar from "./components/Navbar";
+import {BrowserRouter} from "react-router-dom";
+import {useAuthContext} from "./hooks/useAuthContext";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Home from "./pages/Home";
 
 function App() {
+  const {user} = useAuthContext();
   return (
     <main>
-      <Navbar />
-      <div className='w-full h-auto'>
-        <Outlet />
-      </div>
+      <BrowserRouter>
+        <Navbar />
+        <div className='w-full h-auto'>
+          <Routes>
+            <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
+            <Route path='/login' element={!user ? <Login /> : <Navigate to='/' />} />
+            <Route path='/signup' element={!user ? <Signup /> : <Navigate to='/' />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </main>
   );
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,40 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import App from "./App";
-import Home from "./pages/Home";
 import {AuthContextProvider} from "./context/authContext";
 import {WorkoutContextProvider} from "./context/workoutContext";
-import Login from "./pages/Login";
-import Signup from "./pages/Signup";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "/login",
-        element: <Login />,
-      },
-      {
-        path: "/signup",
-        element: <Signup />,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
       <WorkoutContextProvider>
-        <RouterProvider router={router} />
+        <App />
       </WorkoutContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
