4d898af16c7369a748e88e813a5ffa76cbe36607
Arvin Buid <arvinbuid01@gmail.com>
2024-06-14 12:59:04 +0800

    Imported workoutContext dispatch to useLogout custom hook to clear the global state when the user logout of the website.

--- frontend/src/hooks/useLogout.js
+++ frontend/src/hooks/useLogout.js
@@ -1,7 +1,9 @@
 import {useAuthContext} from "./useAuthContext";
+import {useWorkoutsContext} from "./useWorkoutsContext";
 
 export const useLogout = () => {
   const {dispatch} = useAuthContext();
+  const {dispatch: workoutsDispatch} = useWorkoutsContext();
 
   const logout = () => {
     //  remove user from localstorage
@@ -9,6 +11,7 @@ export const useLogout = () => {
 
     // update AuthContext
     dispatch({type: "LOGOUT"});
+    workoutsDispatch({type: "SET_WORKOUTS", payload: null});
   };
 
   return {logout};
