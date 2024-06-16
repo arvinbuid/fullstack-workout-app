5b735baa0bee411fcaa8db23cccc2fb41f0a59a9
Arvin Buid <arvinbuid01@gmail.com>
2024-06-16 18:39:14 +0800

    Modified padding styles inside Login and Signup form components.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -29,7 +29,7 @@ function Login() {
         </div>
         <form
           onSubmit={handleLoginSubmit}
-          className='flex flex-col w-[400px] h-[450px] border-2 border-blue-400 px-10 py-8 rounded-lg bg-white'
+          className='flex flex-col w-[400px] h-auto border-2 border-blue-400 px-10 pt-8 pb-10 rounded-lg bg-white'
         >
           <h3 className='text-3xl font-bold mb-6'>LOGIN</h3>
 
