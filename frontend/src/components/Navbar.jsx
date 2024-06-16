84f402ab0af470622850347a1086004f477fec71
Arvin Buid <arvinbuid01@gmail.com>
2024-06-16 09:10:41 +0800

    login and logout navbar links styles fix.

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,7 +26,7 @@ function Navbar() {
         </Link>
         {/* logout */}
 
-        <div className=' items-center gap-6 tracking-tight'>
+        <div className='flex items-center gap-6 tracking-tight'>
           {user ? (
             <div className='flex items-center gap-4'>
               <p className='text-lg px-2 font-semibold hidden sm:flex'>{user.email}</p>
@@ -40,18 +40,20 @@ function Navbar() {
             </div>
           ) : (
             <>
-              <Link to='/login'>
-                <div className='flex items-center gap-2 text-xl transition transform hover:border-b-2 border-green-600 pb-2 hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none'>
-                  <FaUserCircle className='text-blue-800' />
-                  <p className='font-semibold'>LOGIN</p>
-                </div>
-              </Link>
-              <Link to='/signup'>
-                <div className='flex items-center gap-2 text-xl transition transform hover:border-b-2 border-green-600 pb-2 hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none'>
-                  <FaRegUserCircle className='text-blue-800' />
-                  <p className='font-semibold'>SIGNUP</p>
-                </div>
-              </Link>
+              <div className='hidden sm:flex gap-4  '>
+                <Link to='/login'>
+                  <div className='flex items-center gap-2 text-xl transition transform hover:border-b-2 border-green-600 pb-2 hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none'>
+                    <FaUserCircle className='text-blue-800' />
+                    <p className='font-semibold'>LOGIN</p>
+                  </div>
+                </Link>
+                <Link to='/signup'>
+                  <div className='flex items-center gap-2 text-xl transition transform hover:border-b-2 border-green-600 pb-2 hover:-translate-y-1 motion-reduce:transition-none motion-reduce:hover:transform-none'>
+                    <FaRegUserCircle className='text-blue-800' />
+                    <p className='font-semibold'>SIGNUP</p>
+                  </div>
+                </Link>
+              </div>
             </>
           )}
         </div>
